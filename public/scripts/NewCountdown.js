d0f37bddf7de8d81d66492ad1fc834bd6ad8e5f1
Rasmus Rygaard <rasmus@rasmusrygaard.com>
2012-10-24 18:17:19 -0700

    Used the wonderful placeholder attribute.

--- public/scripts/NewCountdown.js
+++ public/scripts/NewCountdown.js
@@ -1,10 +1,5 @@
 $(document).ready(function () {
 	$('#timezone-field').val(new Date().getTimezoneOffset());
-	var formatter = new FormFormatter('countdown-form');
-	formatter.addField('hour-field', '12');
-	formatter.addField('minute-field', '51');
-	formatter.addField('date-field', '13/07/2013');
-	formatter.addField('description-field', 'Something happens...');
 	$("#date-field").datepicker({
 		onSelect: function(dateText, inst) {
 			formatter.valueAdded('date-field');
